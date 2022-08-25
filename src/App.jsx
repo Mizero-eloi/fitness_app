f6bdc9dabcbb3ff630d7b3775e9f8cd0282193ca
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-08-25 13:03:35 +0200

    initial commits

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import ExerciseDetail from "./components/ExerciseDetail";
+
+const App = () => {
+  return (
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+    </Box>
+  );
+};
+
+export default App;
